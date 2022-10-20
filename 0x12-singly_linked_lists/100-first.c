79c606c7e4d4398cd7252ba0f1c6943e7c906808
Maxiron <naarons800@gmail.com>
2022-10-20 07:47:12 -0700

    fix-3

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,13 +7,4 @@ void __attribute__((constructor)) premain()
 	printf(
 	"You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
 	);
-#include <stdio.h>
-/**
- * premain - runs before main
- */
-void __attribute__((constructor)) premain()
-{
-	printf(
-	"You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
-	);
-}}
+}
